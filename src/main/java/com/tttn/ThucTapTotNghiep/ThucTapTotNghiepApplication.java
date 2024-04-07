3bbbe20693e9f000dd70225f4125a486f0ae941c
daothanhdat <daothanhdat@gmail.com>
2024-04-07 14:00:38 +0700

    aaa

---
+++ src/main/java/com/tttn/ThucTapTotNghiep/ThucTapTotNghiepApplication.java
@@ -0,0 +1,13 @@
+package com.tttn.ThucTapTotNghiep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThucTapTotNghiepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThucTapTotNghiepApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tttn/ThucTapTotNghiep/ThucTapTotNghiepApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tttn.ThucTapTotNghiep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThucTapTotNghiepApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
